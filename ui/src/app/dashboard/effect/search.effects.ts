247b6a80b34eedbe01ff7855a44d9c7f24bece54
Ryan Mathis <rmathis@unicon.net>
2018-04-13 15:12:38 -0700

    Fixing import issue

--- ui/src/app/dashboard/effect/search.effects.ts
+++ ui/src/app/dashboard/effect/search.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/combineLatest';
 
