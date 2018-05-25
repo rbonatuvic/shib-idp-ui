603510354a912af3bfebd32eaf34293f93c5d5f9
Dmitriy Kopylenko <dima767@gmail.com>
2018-05-25 07:22:35 -0400

    [SHIBUI-530]: Add crud repository

---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/repository/EntityRoleWhiteListFilterRepository.java
@@ -0,0 +1,16 @@
+package edu.internet2.tier.shibboleth.admin.ui.repository;
+
+import edu.internet2.tier.shibboleth.admin.ui.domain.filters.EntityRoleWhiteListFilter
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Spring Data CRUD repository for instances of {@link EntityRoleWhiteListFilter}s.
+ */
+public interface EntityRoleWhiteListFilterRepository extends CrudRepository<EntityRoleWhiteListFilter, Long> {
+
+    EntityRoleWhiteListFilter findByName(String name);
+
+    EntityRoleWhiteListFilter findByResourceId(String resourceId);
+
+    boolean deleteByResourceId(String resourceId);
+}
