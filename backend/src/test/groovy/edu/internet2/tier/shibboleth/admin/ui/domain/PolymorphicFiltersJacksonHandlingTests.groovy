c4de070c123311389821640f1609f8d920eb7a3a
Dmitriy Kopylenko <dima767@gmail.com>
2018-05-30 15:39:18 -0400

    Polishing

--- backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/domain/PolymorphicFiltersJacksonHandlingTests.groovy
+++ backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/domain/PolymorphicFiltersJacksonHandlingTests.groovy
@@ -2,41 +2,13 @@ package edu.internet2.tier.shibboleth.admin.ui.domain
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import edu.internet2.tier.shibboleth.admin.ui.configuration.CoreShibUiConfiguration
-import edu.internet2.tier.shibboleth.admin.ui.configuration.MetadataResolverConfiguration
-import edu.internet2.tier.shibboleth.admin.ui.configuration.SearchConfiguration
-import edu.internet2.tier.shibboleth.admin.ui.controller.FilterController
-import edu.internet2.tier.shibboleth.admin.ui.domain.EntityAttributesFilter
+
 import edu.internet2.tier.shibboleth.admin.ui.domain.filters.EntityRoleWhiteListFilter
 import edu.internet2.tier.shibboleth.admin.ui.domain.filters.MetadataFilter
-import edu.internet2.tier.shibboleth.admin.ui.domain.resolvers.MetadataResolver
-import edu.internet2.tier.shibboleth.admin.ui.repository.MetadataResolverRepository
-import edu.internet2.tier.shibboleth.admin.ui.service.FilterService
-import edu.internet2.tier.shibboleth.admin.ui.service.MetadataResolverService
-import edu.internet2.tier.shibboleth.admin.ui.util.RandomGenerator
-import edu.internet2.tier.shibboleth.admin.ui.util.TestObjectGenerator
-import edu.internet2.tier.shibboleth.admin.util.AttributeUtility
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.w3c.dom.Document
+
 import spock.lang.Specification
 
-import static org.hamcrest.CoreMatchers.containsString
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-
 class PolymorphicFiltersJacksonHandlingTests extends Specification {
-
-    @Autowired
-    FilterService filterService
 
     ObjectMapper mapper
 
