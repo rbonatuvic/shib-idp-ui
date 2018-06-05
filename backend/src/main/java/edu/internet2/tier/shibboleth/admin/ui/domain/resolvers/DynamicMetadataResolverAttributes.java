cdb469757e58d6d2e60437635988c901e945754c
Dmitriy Kopylenko <dima767@gmail.com>
2018-06-05 14:21:24 -0400

    SHIBUI-518[535]: Jpa model work in progress

---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/domain/resolvers/DynamicMetadataResolverAttributes.java
@@ -0,0 +1,47 @@
+package edu.internet2.tier.shibboleth.admin.ui.domain.resolvers;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DynamicMetadataResolverAttributes {
+
+    private String parserPoolRef;
+
+    private String taskTimerRef;
+
+    private Double refreshDelayFactor;
+
+    private String minCacheDuration;
+
+    private String maxCacheDuration;
+
+    private String maxIdleEntityData;
+
+    private Boolean removeIdleEntityData;
+
+    private String cleanupTaskInterval;
+
+    private String persistentCacheManagerRef;
+
+    private String persistentCacheManagerDirectory;
+
+    private String persistentCacheKeyGeneratorRef;
+
+    private Boolean initializeFromPersistentCacheInBackground;
+
+    private String backgroundInitializationFromCacheDelay;
+
+    private String initializationFromCachePredicateRef;
+
+}
