f9ee2db888e26b4cf8d8791fc6b09c2e94c60ba0
Jodie Muramoto <jmuramoto@unicon.net>
2018-06-21 09:24:22 -0700

    SHIBUI-572: Added provider wizard and container folder;

---
+++ ui/src/app/metadata/provider/container/wizard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'wizard-page',
+    templateUrl: './wizard.component.html',
+    styleUrls: ['./wizard.component.scss']
+})
+
+export class WizardComponent {
+
+}
+
