f4a7b471f413d0975d2e96440abf2b4346f3f91f
Dmitriy Kopylenko <dima767@gmail.com>
2018-09-11 11:50:15 -0400

    SHIBUI-836 wip

---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/service/SpringPropertyPlaceholderBasedValueResolvingService.java
@@ -0,0 +1,16 @@
+package edu.internet2.tier.shibboleth.admin.ui.service;
+
+/**
+ * Implementation of {@link TokenPlaceholderValueResolvingService} based on Spring Framework's default property resolver
+ * which understands and replaces Shibboleth Idp specific placeholder prefix of '%{' with standard Spring's placeholder
+ * prefix of '${' before resolving.
+ *
+ * If passed it value does not contain Shibboleth Idp '%{}' placeholder token, returns that value as is.
+ */
+public class SpringPropertyPlaceholderBasedValueResolvingService implements TokenPlaceholderValueResolvingService {
+
+    @Override
+    public String resolveValueFromTokenPlaceholder(String tokenPlaceholder) {
+        return null;
+    }
+}
