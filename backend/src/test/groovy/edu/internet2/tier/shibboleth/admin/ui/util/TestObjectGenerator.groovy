640423aad9c2ad6e56e796bf43af9e74c29132a5
Bill Smith <wsmith@unicon.net>
2018-09-12 09:16:49 -0700

    [SHIBUI-570]
    Dropped min refresh down to 30S.

--- backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/util/TestObjectGenerator.groovy
+++ backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/util/TestObjectGenerator.groovy
@@ -460,7 +460,7 @@ class TestObjectGenerator {
             it.metadataURL = 'https://idp.unicon.net/idp/shibboleth'
 
             it.reloadableMetadataResolverAttributes = new ReloadableMetadataResolverAttributes().with {
-                it.minRefreshDelay = 'PT5M'
+                it.minRefreshDelay = 'PT30S'
                 it.maxRefreshDelay = 'PT1H'
                 it.refreshDelayFactor = 0.75
                 it
