72eaf873e06eb2be07ebf3dcc315c1f2bb567da3
Dmitriy Kopylenko <dima767@gmail.com>
2018-09-12 11:38:54 -0400

    SHIBUI-836: clean up

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/ShibbolethUiApplication.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/ShibbolethUiApplication.java
@@ -1,10 +1,7 @@
 package edu.internet2.tier.shibboleth.admin.ui;
 
 import edu.internet2.tier.shibboleth.admin.ui.repository.MetadataResolverRepository;
-import edu.internet2.tier.shibboleth.admin.ui.service.TokenPlaceholderValueResolvingService;
-import edu.internet2.tier.shibboleth.admin.util.TokenPlaceholderResolvers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,8 +10,6 @@ import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.PropertyResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
@@ -41,24 +36,10 @@ public class ShibbolethUiApplication extends SpringBootServletInitializer {
         @Autowired
         MetadataResolverRepository metadataResolverRepository;
 
-        @Value("${idp.home}")
-        String idpHome;
-
-        @Autowired
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer;
-
-        @Autowired
-        PropertyResolver propertyResolver;
-
         @EventListener
         void showMetadataResolversResourceIds(ApplicationStartedEvent e) {
             metadataResolverRepository.findAll()
                     .forEach(it -> System.out.println(String.format("MetadataResolver [%s: %s]", it.getName(), it.getResourceId())));
-
-            System.out.println("IDP HOME: " + idpHome);
-            String rawValue = "%{idp.home}/metadata/file.xml";
-            String resolved = propertyResolver.resolvePlaceholders(rawValue.replace("%{", "${"));
-            TokenPlaceholderValueResolvingService service = TokenPlaceholderResolvers.placeholderResolverService();
         }
     }
 
