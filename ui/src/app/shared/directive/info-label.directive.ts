7b85342fb24f2c38942506db00970753678d9e07
Ryan Mathis <rmathis@unicon.net>
2018-10-03 11:12:26 -0700

    SHIBUI-354 Attempt to fix a11y of popovers

--- ui/src/app/shared/directive/info-label.directive.ts
+++ ui/src/app/shared/directive/info-label.directive.ts
@@ -7,19 +7,10 @@ import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
     providers: [NgbPopover]
 })
 export class InfoLabelDirective {
-    @HostListener('keydown.space') onSpaceDown() {
-        this.config.open();
-    }
-    @HostListener('keyup.space') onSpaceUp() {
-        this.config.close();
-    }
     constructor(
         private config: NgbPopover,
         private element: ElementRef
     ) {
-        config.triggers = 'mouseenter:mouseleave';
-        config.placement = ['top', 'left'];
-        config.container = 'body';
-        element.nativeElement.setAttribute('tabindex', 0);
+        
     }
 }
