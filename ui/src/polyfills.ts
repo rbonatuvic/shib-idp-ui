41c10c5cee60ec628eb496a533c5d9ddfb37a9f3
Ryan Mathis <rmathis@unicon.net>
2018-10-22 09:43:18 -0700

    SHIBUI-921 Added es7 polyfills for array/object methods

--- ui/src/polyfills.ts
+++ ui/src/polyfills.ts
@@ -40,7 +40,8 @@ import 'core-js/es6/set';
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-
+import 'core-js/es7/array';
+import 'core-js/es7/object';
 
 /**
  * Required to support Web Animations `@angular/animation`.
