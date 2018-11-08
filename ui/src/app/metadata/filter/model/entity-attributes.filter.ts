96c53cca89ad2144d972e3027ef0481c48ce3aac
Ryan Mathis <rmathis@unicon.net>
2018-11-08 11:51:38 -0700

    SHIBUI-906 Integrated backend endpoint for filter schema

--- ui/src/app/metadata/filter/model/entity-attributes.filter.ts
+++ ui/src/app/metadata/filter/model/entity-attributes.filter.ts
@@ -4,7 +4,7 @@ import { MetadataFilter } from '../../domain/model';
 export const EntityAttributesFilter: FormDefinition<MetadataFilter> = {
     label: 'EntityAttributes',
     type: 'EntityAttributes',
-    schema: 'assets/schema/filter/entity-attributes.schema.json',
+    schema: '/api/ui/EntityAttributesFilters',
     getValidators(): any {
         const validators = {};
         return validators;
