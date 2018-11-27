3248d54f0d57081be9ed8c1d9dcadab226986def
Ryan Mathis <rmathis@unicon.net>
2018-11-27 09:35:01 -0700

    SHIBUI-953 Fixed issue with validation on organizations

--- ui/src/app/schema-form/service/schema.service.ts
+++ ui/src/app/schema-form/service/schema.service.ts
@@ -18,6 +18,7 @@ export class SchemaService {
         if (!formProperty || !formProperty.parent) {
             return false;
         }
+
         let requiredFields = formProperty.parent.schema.required || [];
         let fieldPath = formProperty.path;
         let controlName = fieldPath.substr(fieldPath.lastIndexOf('/') + 1);
@@ -38,6 +39,28 @@ export class SchemaService {
                 required = !required ? requiredFields.indexOf(controlName) > -1 : required;
             });
         }
+
+        if (!required && formProperty.parent instanceof Object) {
+            const parent = formProperty.parent;
+            const dependencies = parent.schema.dependencies;
+            if (dependencies) {
+                const isDependencyOf = Object.keys(dependencies).filter(d => {
+                    let dep = dependencies[d];
+                    return this.getRequiredDependencies(dep);
+                });
+                const hasActiveDependencies = dependencies.hasOwnProperty(controlName) &&
+                    this.getRequiredDependencies(dependencies[controlName]).filter(
+                        d => parent.value.hasOwnProperty(d)
+                    );
+                const isRequired = isDependencyOf.some(d => parent.value.hasOwnProperty(d) && !!parent.value[d]);
+                required = isRequired || !!hasActiveDependencies.length;
+            }
+        }
+
         return required;
+    }
+
+    getRequiredDependencies(dep: any): string[] {
+        return (dep instanceof Array) ? dep : dep.hasOwnProperty('required') ? dep.required : [];
     }
 }
