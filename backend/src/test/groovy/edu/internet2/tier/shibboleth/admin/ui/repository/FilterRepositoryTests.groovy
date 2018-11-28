3cbc68585ddb87427ed65acf593416f714a3a2e0
Dmitriy Kopylenko <dima767@gmail.com>
2018-11-28 10:55:27 -0500

    SHIBUI-799 Fix test

--- backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/repository/FilterRepositoryTests.groovy
+++ backend/src/test/groovy/edu/internet2/tier/shibboleth/admin/ui/repository/FilterRepositoryTests.groovy
@@ -83,6 +83,6 @@ class FilterRepositoryTests extends Specification {
 
         then:
         persistedFilter.audId > 0L
-        persistedFilter.formats.size() == 4z
+        persistedFilter.formats.size() == 4
     }
 }
