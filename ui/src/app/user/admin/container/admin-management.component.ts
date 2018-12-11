dd9d79694f3f6e97442fc40e253f0eb3cf1aa1d0
Ryan Mathis <rmathis@unicon.net>
2018-12-11 12:02:55 -0700

    Updated user view

--- ui/src/app/user/admin/container/admin-management.component.ts
+++ ui/src/app/user/admin/container/admin-management.component.ts
@@ -1,7 +1,13 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
 
 import * as fromRoot from '../../../app.reducer';
+import * as fromAdmin from '../reducer';
+
+import { UserService } from '../../../core/service/user.service';
+import { LoadAdminRequest, UpdateAdminRequest, RemoveAdminRequest } from '../action/collection.action';
+import { Admin } from '../model/admin';
 
 @Component({
     selector: 'admin-management-page',
@@ -11,7 +17,25 @@ import * as fromRoot from '../../../app.reducer';
 })
 export class AdminManagementPageComponent {
 
+    users$: Observable<Admin[]>;
+    roles$: Observable<string[]> = of(['SUPER_ADMIN', 'DELEGATED_ADMIN']);
+
     constructor(
         private store: Store<fromRoot.State>
-    ) { }
+    ) {
+        this.store.dispatch(new LoadAdminRequest());
+
+        this.users$ = this.store.select(fromAdmin.getAllAdmins);
+    }
+
+    setUserRole(user: Admin, change: string): void {
+        this.store.dispatch(new UpdateAdminRequest({
+            ...user,
+            role: change
+        }));
+    }
+
+    deleteUser(user: string): void {
+        this.store.dispatch(new RemoveAdminRequest(user));
+    }
 }
