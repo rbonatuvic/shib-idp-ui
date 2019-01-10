094cb14e36592d0c578149fbdeaedd871b742ea2
Bill Smith <wsmith@unicon.net>
2019-01-10 09:45:49 -0700

    [SHIBUI-1030]
    Replaced @Value with @Setter based on feedback from Dima.

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/configuration/EmailConfiguration.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/configuration/EmailConfiguration.java
@@ -2,8 +2,9 @@ package edu.internet2.tier.shibboleth.admin.ui.configuration;
 
 import edu.internet2.tier.shibboleth.admin.ui.service.EmailService;
 import edu.internet2.tier.shibboleth.admin.ui.service.EmailServiceImpl;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,15 +22,20 @@ import java.util.Collections;
  * @author Bill Smith (wsmith@unicon.net)
  */
 @Configuration
+@ConfigurationProperties("shibui")
 public class EmailConfiguration {
 
     private static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
 
-    @Value("${shibui.text.email.template.path.prefix:/mail/text/}")
-    private String textEmailTemplatePathPrefix;
+    //Configured via @ConfigurationProperties (using setter method) with 'shibui.text.email.template.path.prefix' property and
+    // default value set here if that property is not explicitly set in application.properties
+    @Setter
+    private String textEmailTemplatePathPrefix = "/mail/text/";
 
-    @Value("${shibui.html.email.template.path.prefix:/mail/html/}")
-    private String htmlEmailTemplatePathPrefix;
+    //Configured via @ConfigurationProperties (using setter method) with 'shibui.html.email.template.path.prefix' property and
+    // default value set here if that property is not explicitly set in application.properties
+    @Setter
+    private String htmlEmailTemplatePathPrefix = "/mail/html/";
 
     @Autowired
     private ApplicationContext applicationContext;
