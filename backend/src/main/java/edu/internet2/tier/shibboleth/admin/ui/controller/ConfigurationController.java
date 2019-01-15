0ad7ccb7abc9d9b144bd638809c7bdc838d89047
Bill Smith <wsmith@unicon.net>
2019-01-15 09:14:44 -0700

    [SHIBUI-1030]
    Removed unused imports.

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/controller/ConfigurationController.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/controller/ConfigurationController.java
@@ -7,11 +7,9 @@ import edu.internet2.tier.shibboleth.admin.ui.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.mail.MessagingException;
 import java.util.stream.Collectors;
 
 /**
