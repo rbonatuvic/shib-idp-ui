6c403585cde2c398a473eedeb27340a491dbe41c
Ryan Mathis <rmathis@unicon.net>
2019-01-22 08:08:10 -0700

    Fixes missing required message

--- ui/src/app/schema-form/widget/string/string.component.ts
+++ ui/src/app/schema-form/widget/string/string.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { StringWidget } from 'ngx-schema-form';
 import { Validators } from '@angular/forms';
 import { SchemaService } from '../../service/schema.service';
+import { startWith } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'custom-string',
     templateUrl: `./string.component.html`,
     styleUrls: ['../widget.component.scss']
 })
-export class CustomStringComponent extends StringWidget {
+export class CustomStringComponent extends StringWidget implements AfterViewInit, OnDestroy {
+
+    errorSub: Subscription;
 
     constructor(
         private widgetService: SchemaService
@@ -16,7 +20,22 @@ export class CustomStringComponent extends StringWidget {
         super();
     }
 
+    ngAfterViewInit(): void {
+        super.ngAfterViewInit();
+        this.errorSub = this.control.valueChanges.pipe(startWith(this.control.value)).subscribe(v => {
+            if (!v && this.required && this.errorMessages.some(msg => !!msg.toLowerCase().match('required').length)) {
+                this.errorMessages.push('message.required');
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.errorSub.unsubscribe();
+    }
+
     get required(): boolean {
-        return this.widgetService.isRequired(this.formProperty);
+        const req = this.widgetService.isRequired(this.formProperty);
+
+        return req;
     }
 }
