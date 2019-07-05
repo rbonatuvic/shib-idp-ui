7c74fe803b584921af4050c0b1150d26b7916fb5
Jj! <jj@unicon.net>
2019-07-05 13:53:32 -0500

    [SHIBUI-1263] remove Filter versioning since it is rolling up into the resolver

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/domain/AbstractAttributeExtensibleXMLObject.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/domain/AbstractAttributeExtensibleXMLObject.java
@@ -5,17 +5,21 @@ import org.opensaml.core.xml.AttributeExtensibleXMLObject;
 import org.opensaml.core.xml.util.AttributeMap;
 
 import javax.annotation.Nonnull;
+import javax.persistence.ElementCollection;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
 import javax.persistence.Transient;
+import javax.xml.namespace.QName;
+import java.util.HashMap;
+import java.util.Map;
 
 @MappedSuperclass
-@EqualsAndHashCode(callSuper = true, exclude={"unknownAttributes"})
+@EqualsAndHashCode(callSuper = true, exclude={"storageAttributeMap"})
 public abstract class AbstractAttributeExtensibleXMLObject extends AbstractXMLObject implements AttributeExtensibleXMLObject {
-
-    private transient final AttributeMap unknownAttributes;
+    private transient final AttributeMap unknownAttributes = new AttributeMap(this);
 
     AbstractAttributeExtensibleXMLObject() {
-        unknownAttributes = new AttributeMap(this);
     }
 
     @Nonnull
@@ -23,5 +27,18 @@ public abstract class AbstractAttributeExtensibleXMLObject extends AbstractXMLOb
     @Transient
     public AttributeMap getUnknownAttributes() {
         return this.unknownAttributes;
+    }
+
+    @ElementCollection
+    private Map<QName,String> storageAttributeMap = new HashMap<>();
+
+    @PrePersist
+    void prePersist() {
+        this.storageAttributeMap = this.unknownAttributes;
+    }
+
+    @PostLoad
+    void postLoad() {
+        this.unknownAttributes.putAll(this.storageAttributeMap);
     }
 }
---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/domain/DigestMethod.java
@@ -0,0 +1,29 @@
+package edu.internet2.tier.shibboleth.admin.ui.domain;
+
+import lombok.EqualsAndHashCode;
+
+import javax.annotation.Nullable;
+import javax.persistence.Entity;
+
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class DigestMethod extends AbstractElementExtensibleXMLObject implements org.opensaml.saml.ext.saml2alg.DigestMethod {
+    private String algorithm;
+
+    public DigestMethod() {}
+
+    public DigestMethod(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    @Nullable
+    @Override
+    public String getAlgorithm() {
+        return null;
+    }
+
+    @Override
+    public void setAlgorithm(@Nullable String value) {
+
+    }
+}
