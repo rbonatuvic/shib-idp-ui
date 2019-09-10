f36f573954f41723b1ae3393e64efd25aa3e9148
Ryan Mathis <rmathis@unicon.net>
2019-09-10 08:23:28 -0700

    SHIBUI-1407 Implemented differences highlighting

--- ui/src/app/metadata/configuration/component/property/array-property.component.ts
+++ ui/src/app/metadata/configuration/component/property/array-property.component.ts
@@ -33,6 +33,13 @@ export class ArrayPropertyComponent extends ConfigurationPropertyComponent imple
         return UriValidator.isUri(str);
     }
 
+    isDifferent(key: string): boolean {
+        const model = this.property.value || [];
+        return model
+            .map((value) => value ? value.indexOf(key) > -1 : false)
+            .reduce((current, val) => current !== val ? true : false, false);
+    }
+
     get attributeList$(): Observable<{ key: string, label: string }[]> {
         if (this.property.widget && this.property.widget.hasOwnProperty('data')) {
             return of(this.property.widget.data);
