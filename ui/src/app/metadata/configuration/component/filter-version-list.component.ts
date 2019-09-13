1052fd988ad0876188cc248107c44bdd61323822
Ryan Mathis <rmathis@unicon.net>
2019-09-13 13:17:41 -0700

    SHIBUI-1364 Adding version comparison

--- ui/src/app/metadata/configuration/component/filter-version-list.component.ts
+++ ui/src/app/metadata/configuration/component/filter-version-list.component.ts
@@ -1,52 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FilterConfiguration } from '../model/metadata-configuration';
 
-const data = {
-    dates: ['2019-08-08T08:40:32.015', '2019-08-08T08:40:19.266'],
-    filters: [
-        [
-            {
-                id: '1',
-                name: 'Example Filter 1',
-                type: 'EntityAttributesFilter',
-                comparable: true
-            },
-            {
-                id: '2',
-                name: 'Example Filter 2',
-                type: 'NameIdFilter',
-                comparable: true
-            }
-        ],
-        [
-            {
-                id: '2',
-                name: 'Example Filter 2',
-                type: 'NameIdFilter',
-                comparable: true
-            },
-            {
-                id: '1',
-                name: 'Example Filter 1',
-                type: 'EntityAttributesFilter',
-                comparable: true
-            }
-        ],
-        [
-            {
-                id: '4',
-                name: 'Example Filter 4',
-                type: 'EntityAttributesFilter',
-                comparable: false
-            },
-            {
-                id: '3',
-                name: 'Example Filter 3',
-                type: 'EntityAttributesFilter',
-                comparable: false
-            }
-        ]
-    ]
-};
+
 
 @Component({
     selector: 'filter-version-list',
@@ -54,7 +9,7 @@ const data = {
 })
 export class FilterVersionListComponent {
 
-    filters = data;
+    @Input() filters: FilterConfiguration;
     selected: string;
     comparing: string;
 
--- ui/src/app/metadata/configuration/model/metadata-configuration.ts
+++ ui/src/app/metadata/configuration/model/metadata-configuration.ts
@@ -1,7 +1,12 @@
 import { Section } from './section';
-import { Metadata } from '../../domain/domain.type';
+import { FilterVersion } from './version';
 
 export interface MetadataConfiguration {
     sections: Section[];
     dates: String[];
+}
+
+export interface FilterConfiguration {
+    dates: ['2019-08-08T08:40:32.015', '2019-08-08T08:40:19.266'];
+    filters: FilterVersion[];
 }
