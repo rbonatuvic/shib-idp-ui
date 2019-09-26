20a0534cdb5e491e86b105efe7263d7c3cd28b34
Bill Smith <wsmith@unicon.net>
2019-09-26 10:32:48 -0700

    SHIBUI-1364
    Committing selenium scripts with issues for Ryan to review. See
    SHIBUI-1509 and SHIBUI-1510.

--- backend/src/integration/groovy/edu/internet2/tier/shibboleth/admin/ui/SeleniumSIDETest.groovy
+++ backend/src/integration/groovy/edu/internet2/tier/shibboleth/admin/ui/SeleniumSIDETest.groovy
@@ -108,7 +108,9 @@ class SeleniumSIDETest extends Specification {
 //        'SHIBUI-1407: Metadata provider comparison highlights'              | '/SHIBUI-1407-2.side'
 //        'SHIBUI-1364: Compare FBHTTPMP with filters'                        | '/SHIBUI-1364-1.side'
 //        'SHIBUI-1364: Compare FSMP with filters'                            | '/SHIBUI-1364-2.side'
-        'SHIBUI-1364: Compare LDMP with filters'                            | '/SHIBUI-1364-3.side'
+//        'SHIBUI-1364: Compare LDMP with filters'                            | '/SHIBUI-1364-3.side'
+//        'SHIBUI-1364: Compare DHTTPMP with filters'                         | '/SHIBUI-1364-4.side'
+        'SHIBUI-1364: DELETE ME BEFORE PR'                                  | '/false.side'
     }
 }
 
