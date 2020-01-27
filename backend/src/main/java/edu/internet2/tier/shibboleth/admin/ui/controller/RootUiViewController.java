0fab57f6f28625d45e4fc456f9d5818b0c25cde6
Dmitriy Kopylenko <dima767@gmail.com>
2020-01-27 12:05:32 -0500

    Working solution

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/controller/RootUiViewController.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/controller/RootUiViewController.java
@@ -10,28 +10,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 @Controller
 public class RootUiViewController {
 
-    @Value("classpath:/resources/index.html")
-    Resource indexHtmlResource;
-
     @RequestMapping("/")
-    //@ResponseBody
-    public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //TODO: Use Jsoup lib to inject context path into DOM before writing to response buffer
+    public String index() {
+        return "redirect:/index.html";
+    }
 
-        final String ctxPath = request.getContextPath();
+    @RequestMapping(value = {"**/index.html", "/dashboard/**", "/metadata/**"})
+    public void indexHtml(HttpServletRequest request, HttpServletResponse response) throws IOException, URISyntaxException {
+        //This method is necessary in order for Angular framework to honor dynamic ServletContext
+        //under which shib ui application is deployed, both during initial index.html load and subsequest page refreshes
+        String content = new BufferedReader(new InputStreamReader(request.getServletContext()
+                .getResourceAsStream("/WEB-INF/classes/resources/index.html")))
+                .lines()
+                .collect(Collectors.joining("\n"));
 
-        //This does not work! In order form Angular to kick in we need to redirect to index.html
-        String indexHtmlAsString = StreamUtils.copyToString(indexHtmlResource.getInputStream(), Charset.defaultCharset());
-        //byte[] indexHtmlBytes = ByteStreams.toByteArray(indexHtmlResource.getInputStream());
-        response.setHeader("Content-Type", "text/html");
-        response.getWriter().print(indexHtmlAsString);
-        //os.write(ctxPath.getBytes(), 0, ctxPath.length());
+        content = content.replaceFirst("<base.+>", "<base href=\"" + request.getContextPath() + "/\">");
+        response.setContentType("text/html");
+        try (OutputStream writer = response.getOutputStream()) {
+            writer.write(content.getBytes());
+        }
     }
 }
