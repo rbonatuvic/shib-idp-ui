4a8cfe6f7d8e63be2ff0163782646770ae6b9077
chasegawa <chasegawa@unicon.net>
2021-08-08 11:41:12 -0700

    SHIBUI-2003
    First pass - ownership items

---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/security/model/Ownable.java
@@ -0,0 +1,13 @@
+package edu.internet2.tier.shibboleth.admin.ui.security.model;
+
+public interface Ownable {
+    /**
+     * @return representation of the id of the object. This is likely (but not limited to) the resource id of the object
+     */
+    public String getObjectId();
+
+    /**
+     * @return the OwnableType that describes the Ownable object
+     */
+    public OwnableType getOwnableType();
+}
---
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/security/repository/OwnershipRepository.java
@@ -0,0 +1,59 @@
+package edu.internet2.tier.shibboleth.admin.ui.security.repository;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import edu.internet2.tier.shibboleth.admin.ui.security.model.Ownable;
+import edu.internet2.tier.shibboleth.admin.ui.security.model.Owner;
+import edu.internet2.tier.shibboleth.admin.ui.security.model.Ownership;
+
+public interface OwnershipRepository extends JpaRepository<Ownership, String> {
+    /**
+     * Delete the user from any groups they may in.
+     */
+    @Query("DELETE FROM ownership o WHERE o.ownedId = :username AND o.ownedType = 'USER' AND o.ownerType = 'GROUP'")
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    void clearUsersGroups(@Param("username") String username);
+
+    /**
+     * Remove any ownership of the ownable object
+     */
+    @Query("DELETE FROM ownership o WHERE o.ownedId = :#{#ownedObject.getObjectId()} AND o.ownedType = :#{#ownedObject.getOwnableType().toString()}")
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    void deleteEntriesForOwnedObject(@Param("ownedObject") Ownable ownedObject);
+
+    /**
+     * Find all items an owner owns
+     */
+    @Query("SELECT o FROM ownership o WHERE o.ownerId = :#{#owner.getOwnerId()} AND o.ownerType = :#{#owner.getOwnerType().toString()}")
+    Set<Ownership> findAllByOwner(@Param("owner") Owner owner);
+
+    /**
+     * Find all the groups that a user belongs to
+     */
+    @Query("SELECT o FROM ownership o WHERE o.ownedId = :username AND o.ownedType = 'USER' AND o.ownerType = 'GROUP' ")
+    Set<Ownership> findAllGroupsForUser(@Param("username") String username);
+
+    /**
+     * Find the owner of this object
+     */
+    @Query("SELECT o FROM ownership o WHERE o.ownedId = :#{#ownedObject.getObjectId()} AND o.ownedType = :#{#ownedObject.getOwnableType().toString()}")
+    Set<Ownership> findOwnableObjectOwners(@Param("ownedObject") Ownable ownedObject);
+
+    /**
+     * Find all things the user owns
+     */
+    @Query("SELECT o FROM ownership o WHERE o.ownerId = :username AND o.ownerType = 'USER' ")
+    List<Ownership> findOwnedByUser(@Param("username") String username);
+
+    /**
+     * Find only the users that the owner owns
+     */
+    @Query("SELECT o FROM ownership o WHERE o.ownerId = :#{#owner.getOwnerId()} AND o.ownerType = :#{#owner.getOwnerType().toString()} AND o.ownedType='USER'")
+    Set<Ownership> findUsersByOwner(@Param("owner") Owner owner);
+}
