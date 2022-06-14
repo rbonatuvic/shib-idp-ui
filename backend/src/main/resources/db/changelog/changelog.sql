94b4706ebc4731b75a4a295e959a853bce9345ae
chasegawa <chasegawa@unicon.net>
2022-06-14 15:41:48 -0700

    SHIBUI-2273
    Setup liquidbase to run for SQL SERVER
    
    
    Former-commit-id: c5f6475bf52751df2421cdb33266b375d89aa02a

---
+++ backend/src/main/resources/db/changelog/changelog.sql
@@ -0,0 +1,29 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:1.11.0 dbms:mariadb,mysql,postgresql
+-- preconditions onFail:MARK_RAN
+-- precondition-sql-check expectedResult:1 SELECT 1 FROM users
+-- comment: /* we don't need to run this if the system is new */
+ALTER TABLE resource_backed_metadata_resolver
+    RENAME file TO file_resource;
+GO
+ALTER TABLE organizationurl
+    RENAME 'value' TO 'uri_value';
+GO
+
+-- changeset liquibase:1.11.0 dbms:mssql
+-- preconditions onFail:MARK_RAN
+-- precondition-sql-check expectedResult:1 SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'users'
+-- comment: /* we don't need to run this if the system is new */
+EXEC sp_rename 'organizationurl.value', uri_value, 'COLUMN'
+GO
+EXEC sp_rename 'custom_entity_attr_list_items.value', item_value, 'COLUMN'
+GO
+EXEC sp_rename 'organization_name.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'organization_display_name.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'service_description.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'service_name.value', name_value, 'COLUMN'
+GO
