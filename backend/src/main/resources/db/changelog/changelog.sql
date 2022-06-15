ef900fc628311365b7d5cae9e31b5cb45a1fab6f
chasegawa <chasegawa@unicon.net>
2022-06-15 10:05:10 -0700

    SHIBUI-2273
    Updates: added missing tables and aud tables to the sql updates, updated the springboot and hibernate versions to newer versions, updated the settings to work with the newer version of springboot which uses a different library for path pattern matching.

--- backend/src/main/resources/db/changelog/changelog.sql
+++ backend/src/main/resources/db/changelog/changelog.sql
@@ -4,38 +4,95 @@
 -- preconditions onFail:MARK_RAN
 -- precondition-sql-check expectedResult:1 SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'users'
 -- comment: /* we don't need to run this if the system is new */
-ALTER TABLE resource_backed_metadata_resolver
-    RENAME "file" TO "file_resource";
+ALTER TABLE custom_entity_attr_list_items
+    RENAME "value" TO "item_value";
+GO
+ALTER TABLE custom_entity_attr_list_items_aud
+    RENAME "value" TO "item_value";
+GO
+ALTER TABLE entity_attributes_filter_target_value
+    RENAME "value" TO "target_value";
+GO
+ALTER TABLE entity_attributes_filter_target_value_aud
+    RENAME "value" TO "target_value";
+GO
+ALTER TABLE name_id_format_filter_target_value
+    RENAME "value" TO "target_value";
+GO
+ALTER TABLE name_id_format_filter_target_value_aud
+    RENAME "value" TO "target_value";
 GO
 ALTER TABLE organizationurl
     RENAME "value" TO "uri_value";
 GO
-ALTER TABLE organization_name
-    RENAME "value" TO "name_value";
+ALTER TABLE organizationurl_aud
+    RENAME "value" TO "uri_value";
 GO
 ALTER TABLE organization_display_name
     RENAME "value" TO "name_value";
 GO
+ALTER TABLE organization_display_name_aud
+    RENAME "value" TO "name_value";
+GO
+ALTER TABLE organization_name
+    RENAME "value" TO "name_value";
+GO
+ALTER TABLE organization_name_aud
+    RENAME "value" TO "name_value";
+GO
+ALTER TABLE resource_backed_metadata_resolver
+    RENAME "file" TO "file_resource";
+GO
+ALTER TABLE resource_backed_metadata_resolver_aud
+    RENAME "file" TO "file_resource";
+GO
 ALTER TABLE service_description
+    RENAME "value" TO "name_value";
+GO
+ALTER TABLE service_description_aud
     RENAME "value" TO "name_value";
 GO
 ALTER TABLE service_name
     RENAME "value" TO "name_value";
 GO
+ALTER TABLE service_name_aud
+    RENAME "value" TO "name_value";
+GO
+
 
 -- changeset liquibase:1.11.0 dbms:mssql
 -- preconditions onFail:MARK_RAN
 -- precondition-sql-check expectedResult:1 SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'users'
 -- comment: /* we don't need to run this if the system is new */
-EXEC sp_rename 'organizationurl.value', uri_value, 'COLUMN'
-GO
 EXEC sp_rename 'custom_entity_attr_list_items.value', item_value, 'COLUMN'
 GO
-EXEC sp_rename 'organization_name.value', name_value, 'COLUMN'
+EXEC sp_rename 'custom_entity_attr_list_items_aud.value', item_value, 'COLUMN'
+GO
+EXEC sp_rename 'entity_attributes_filter_target_value.value', target_value, 'COLUMN'
+GO
+EXEC sp_rename 'entity_attributes_filter_target_value_aud.value', target_value, 'COLUMN'
+GO
+EXEC sp_rename 'name_id_format_filter_target_value', target_value, 'COLUMN'
+GO
+EXEC sp_rename 'name_id_format_filter_target_value_aud', target_value, 'COLUMN'
+GO
+EXEC sp_rename 'organizationurl.value', uri_value, 'COLUMN'
+GO
+EXEC sp_rename 'organizationurl_aud.value', uri_value, 'COLUMN'
 GO
 EXEC sp_rename 'organization_display_name.value', name_value, 'COLUMN'
 GO
+EXEC sp_rename 'organization_display_name_aud.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'organization_name.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'organization_name_aud.value', name_value, 'COLUMN'
+GO
 EXEC sp_rename 'service_description.value', name_value, 'COLUMN'
 GO
+EXEC sp_rename 'service_description_aud.value', name_value, 'COLUMN'
+GO
 EXEC sp_rename 'service_name.value', name_value, 'COLUMN'
+GO
+EXEC sp_rename 'service_name_aud.value', name_value, 'COLUMN'
 GO
