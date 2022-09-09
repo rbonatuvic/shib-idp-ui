4638b29f18523f2d6e209418aad649d6b52a3d27
Ryan Mathis <rmathis@unicon.net>
2022-09-09 14:32:14 -0700

    Implemented fix for group change and copy change

--- ui/src/app/metadata/copy/EntityTypeahead.js
+++ ui/src/app/metadata/copy/EntityTypeahead.js
@@ -1,10 +1,16 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import { useController } from 'react-hook-form';
-import { useMetadataSources } from '../hooks/api';
+import { useMetadataEntity, useMetadataSources } from '../hooks/api';
+import Form from 'react-bootstrap/Form';
 
-export function EntityTypeahead ({id, control, name}) {
+export function EntityTypeahead ({id, control, name, children}) {
     const { data = [] } = useMetadataSources({}, []);
+
+    const { get, response, loading } = useMetadataEntity();
+
     const entities = React.useMemo(() => data.map(d => d.entityId), [data]);
 
     const {
@@ -18,14 +24,30 @@ export function EntityTypeahead ({id, control, name}) {
         defaultValue: "",
     });
 
+    const loadSelected = async (selected) => {
+        const toCopy = data.find(e => e.entityId === selected);
+
+        const source = await get(`/${toCopy.id}`);
+        if (response.ok) {
+            onChange(source);
+        }
+        //
+    }
+
     return (
-        <Typeahead
-            {...inputProps}
-            onChange={(selected) => onChange(selected ? data.find(e => e.entityId === selected[0]) : '')}
-            defaultInputValue={value ? value.entityId : ''}
-            options={entities}
-            required={true}
-            id={id}
-        />
+        <React.Fragment>
+            <Form.Label htmlFor="target">
+                {children}
+                {loading && <FontAwesomeIcon icon={faSpinner} size="lg" spin={true} pulse={true} className="ms-2" /> }
+            </Form.Label>
+            <Typeahead
+                {...inputProps}
+                onChange={(selected) => selected && selected.length > 0 ? loadSelected(selected[0]) : null}
+                defaultInputValue={value ? value.entityId : ''}
+                options={entities}
+                required={true}
+                id={id}
+            />
+        </React.Fragment>
     )
 }
