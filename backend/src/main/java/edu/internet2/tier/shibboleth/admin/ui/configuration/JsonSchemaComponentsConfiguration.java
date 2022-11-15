3e7e9bbeabab9891bcadb194151ad39e93b2d625
chasegawa <chasegawa@unicon.net>
2022-11-14 17:49:03 -0700

    SHIBUI-2393
    api/ui/DynamicRegistration endpoint to deliver json schema for the frontend

--- backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/configuration/JsonSchemaComponentsConfiguration.java
+++ backend/src/main/java/edu/internet2/tier/shibboleth/admin/ui/configuration/JsonSchemaComponentsConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.ResourceLoader;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.JsonSchemaLocationBuilder;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.ALGORITHM_FILTER;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.DYNAMIC_HTTP_METADATA_RESOLVER;
+import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.DYNAMIC_REGISTRATION;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.ENTITY_ATTRIBUTES_FILTERS;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.EXTERNAL_METADATA_RESOLVER;
 import static edu.internet2.tier.shibboleth.admin.ui.jsonschema.JsonSchemaResourceLocation.SchemaType.FILESYSTEM_METADATA_RESOLVER;
@@ -73,6 +74,11 @@ public class JsonSchemaComponentsConfiguration {
     @Setter
     private String algorithmFilterUiSchemaLocation = "classpath:algorithm-filter.schema.json";
 
+    //Configured via @ConfigurationProperties (using setter method) with 'shibui.dynamic-registration-ui-schema-location' property and
+    // default value set here if that property is not explicitly set in application.properties
+    @Setter
+    private String dynamicRegistrationUiSchemaLocation = "classpath:dynamic-registration.schema.json";
+
     @Bean
     public JsonSchemaResourceLocationRegistry jsonSchemaResourceLocationRegistry(ResourceLoader resourceLoader, ObjectMapper jacksonMapper) {
         return JsonSchemaResourceLocationRegistry.inMemory()
@@ -126,6 +132,12 @@ public class JsonSchemaComponentsConfiguration {
                         .build())
                 .register(ALGORITHM_FILTER, JsonSchemaLocationBuilder.with()
                         .jsonSchemaLocation(algorithmFilterUiSchemaLocation)
+                        .resourceLoader(resourceLoader)
+                        .jacksonMapper(jacksonMapper)
+                        .detectMalformedJson(true)
+                        .build())
+                .register(DYNAMIC_REGISTRATION, JsonSchemaLocationBuilder.with()
+                        .jsonSchemaLocation(dynamicRegistrationUiSchemaLocation)
                         .resourceLoader(resourceLoader)
                         .jacksonMapper(jacksonMapper)
                         .detectMalformedJson(true)
